aa7c9af9136fbbf980a30439afb04d2b9e80f307
Phillip Tennen <phillip.ennen@gmail.com>
2016-06-10 17:00:12 -0600

    Locked program allows retries

--- locked.c
+++ locked.c
@@ -3,21 +3,31 @@
 
 #define PASSWORD "b"
 
-int main(int argc, char** argv) {
-	char word[256];
-	fgets(word, sizeof(word), stdin);
-	
-	//remove newline from input
-	char* pos;
-	if ((pos = strchr(word, '\n')) != NULL) {
-		*pos = '\0';
-	}
-
+int process_input(char* word) {
 	//check for correct password
 	if (strcmp(word, PASSWORD) == 0) {
 		printf("Access granted.\n");
-		return 0;
+		return 1;
 	}
 	printf("Incorrect password.\n");
-	return 1;
+	return 0;
+}
+
+int main(int argc, char** argv) {
+	int access_granted = 0;
+	while (!access_granted) {
+		//get input 
+		char word[256];
+		fgets(word, sizeof(word), stdin);
+		
+		//remove newline from input
+		char* pos;
+		if ((pos = strchr(word, '\n')) != NULL) {
+			*pos = '\0';
+		}
+
+		//test input
+		access_granted = process_input(word);
+	}
+	return 0;
 }
