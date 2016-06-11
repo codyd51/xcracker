af04f4fcd062d32a2e85814031eae36d56c3ee27
Phillip Tennen <phillip.ennen@gmail.com>
2016-06-10 19:42:00 -0600

    Implement cracking for multi-character passwords; architectural changes

--- cracker.c
+++ cracker.c
@@ -4,44 +4,44 @@
 #include <sys/types.h>
 #include <stdlib.h>
 
-char* stringgen(int length) {
-	static int ch = 32;
+#define MAX_LENGTH 16
 
-	char res[2] = "\0";
-
-	res[0] = ch;
-
-	ch++;
-	//if we reached the end of printable characters, cycle back to the beginning
-	if (ch == 127) ch = 32;
-	//the following characters trip us up, so if we're on one then skip it
-	if (ch == '\"') ch++;
-	if (ch == '&') ch++;
-	if (ch == '\\') ch++;
-	if (ch == '`') ch++;
-
-	//convert char[] to char*
-	char* ret= malloc((length + 1) * sizeof(char));
-	ret = strncpy(ret, res, length);
-	
-	return ret;
+int inc(char* c) {
+	if (c[0] == 0) return 0;
+	if (c[0] == 'z') {
+		c[0] = 'a';
+		return inc(c + sizeof(char));
+	}
+	c[0]++;
+	return 1;
 }
 
 int main(int argc, char** argv) {
-	for (int i = 0; i < (127-32); i++) {
-		char* guess = stringgen(1);
+	static int tries = 0;
+	for (int length = 1; length <= 2; length++) {
 
-		char command[512];
-		sprintf(command, "./locked \"%s\"", guess);
-		int status = system(command);
-		if (!status) {
-			//success! we found the correct password
-			printf("\n");
-			printf("---done---\n");
-			printf("Password: %s\n", guess);
-			printf("Found in %d tries\n", i);
-			return 0;
+		char* guess = malloc((length + 1) * sizeof(char));
+		for (int i = 1; i <= length; i++) {
+			for (int j = 0; j < i; j++) {
+				guess[j] = 'a';
+			}
+			guess[i] = 0;
 		}
+		do {
+			tries++;
+
+			char command[512];
+			sprintf(command, "./locked \"%s\"", guess);
+			int status = system(command);
+			if (!status) {
+				//success! we found the correct password
+				printf("\n");
+				printf("---done---\n");
+				printf("Password: %s\n", guess);
+				printf("Found in %d tries\n", tries);
+				return 0;
+			}
+		} while(inc(guess));
 	}
 
 	//out of guesses
