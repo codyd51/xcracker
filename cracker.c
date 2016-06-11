4d03c1eacf957fb207a36ce8610881244e5ce015
Phillip Tennen <phillip.ennen@gmail.com>
2016-06-10 18:07:36 -0600

    Implement basic cracker to run through ASCII on locked program and check if password was correct

--- cracker.c
+++ cracker.c
@@ -1,25 +1,47 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
 
 char* stringgen(int length) {
-	static int ch = 'a';
+	static int ch = 32;
 
-	char res[length+1];
+	char res[2] = "\0";
 
 	res[0] = ch;
-	res[1] = '\0';
 
-	//if we reached the end of the alphabet, cycle back to the beginning
-	if (ch == 'z') ch = 'a';
+	ch++;
+	//if we reached the end of printable characters, cycle back to the beginning
+	if (ch == 127) ch = 32;
+	//the following characters trip us up, so if we're on one then skip it
+	//quote
+	if (ch == 34) ch++;
+	//ampersand
+	if (ch == 38) ch++;
 
 	//convert char[] to char*
-	char* ret = &res[0];
+	char* ret= malloc((length + 1) * sizeof(char));
+	ret = strncpy(ret, res, length);
+	
 	return ret;
 }
 
 int main(int argc, char** argv) {
-	for (int i = 0; i < ('z' - 'a'); i++) {
+	for (int i = 0; i < (127-32); i++) {
 		char* guess = stringgen(1);
 
+		char command[512];
+		sprintf(command, "./locked \"%s\"", guess);
+		int status = system(command);
+		if (!status) {
+			//success! we found the correct password
+			printf("The password is %s\n", guess);
+			return 0;
+		}
 	}
+
+	//out of guesses
+	printf("Couldn't find password.");
+	return 1;
 }
