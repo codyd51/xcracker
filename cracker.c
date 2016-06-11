6e5450b4fc5ac4cb12316a13f81bd840a0c477f9
Phillip Tennen <phillip.ennen@gmail.com>
2016-06-10 22:38:00 -0600

    Factor out password attempt and track time taken to crack

--- cracker.c
+++ cracker.c
@@ -4,11 +4,13 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <time.h>
 
 #define MAX_LENGTH 16
 
 int inc(char* c) {
 	if (c[0] == 0) return 0;
+
 	if (c[0] == 'z') {
 		c[0] = 'a';
 		return inc(c + sizeof(char));
@@ -17,7 +19,15 @@ int inc(char* c) {
 	return 1;
 }
 
+int attempt_password(char* pass) {
+	char input[strlen(pass) + 1];
+	sprintf(input, "./locked \"%s\"\n", pass);
+	return system(input);
+}
+
 int main(int argc, char** argv) {
+	clock_t begin = clock();
+
 	for (int length = 1; length <= MAX_LENGTH; length++) {
 		char* guess = malloc((length + 1) * sizeof(char));
 		for (int i = 1; i <= length; i++) {
@@ -29,15 +39,16 @@ int main(int argc, char** argv) {
 		do {
 			static int tries = 0;
 			tries++;
-
-			char input[strlen(guess) + 1];
-			sprintf(input, "./locked %s\n", guess);
-			if (!system(input)) {
+			if (!attempt_password(guess)) {
 				//success! we found the correct password
 				printf("\n");
 				printf("---done---\n");
 				printf("Password: %s\n", guess);
 				printf("Found in %d attempts\n", tries);
+
+				clock_t end = clock();
+				double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+				printf("Completed in %f\n", time_spent);
 				return 0;
 			}
 		} while(inc(guess));
