935a6a2ea60ff00f1532480de5e583a0425c3d3f
Phillip Tennen <phillip.ennen@gmail.com>
2016-06-12 20:50:38 -0600

    Implement dictionary attack

--- cracker.c
+++ cracker.c
@@ -6,7 +6,44 @@
 #include <signal.h>
 #include <time.h>
 
-#define MAX_LENGTH 16
+#define MAX_LENGTH 4
+
+int attempt_password(char* pass) {
+	char input[strlen(pass) + 1];
+	sprintf(input, "./locked \"%s\"\n", pass);
+	return system(input);
+}
+
+static int tries = 0;
+
+int dictionary_crack() {
+	FILE* file = fopen("common_pwds.txt", "r");
+	char line[256];
+
+	while (fgets(line, sizeof(line), file)) {
+		tries++;
+
+		//strip newline
+		char* pos;
+		if ((pos = strchr(line, '\n')) != NULL) {
+			*pos = '\0';
+		}
+
+		if (!attempt_password(line)) {
+			//found correct password!
+			printf("\n---done---\n");
+			printf("Password: %s\n", line);
+			printf("Found in %d attempts\n", tries);
+
+			fclose(file);
+
+			return 0;
+		}
+	}
+	//couldn't find password!
+	fclose(file);
+	return 1;
+}
 
 int inc(char* c) {
 	if (c[0] == 0) return 0;
@@ -19,15 +56,7 @@ int inc(char* c) {
 	return 1;
 }
 
-int attempt_password(char* pass) {
-	char input[strlen(pass) + 1];
-	sprintf(input, "./locked \"%s\"\n", pass);
-	return system(input);
-}
-
-int main(int argc, char** argv) {
-	clock_t begin = clock();
-
+int brute_force_crack() {
 	for (int length = 1; length <= MAX_LENGTH; length++) {
 		char* guess = malloc((length + 1) * sizeof(char));
 		for (int i = 1; i <= length; i++) {
@@ -37,22 +66,41 @@ int main(int argc, char** argv) {
 			guess[i] = 0;
 		}
 		do {
-			static int tries = 0;
 			tries++;
 			if (!attempt_password(guess)) {
 				//success! we found the correct password
-				printf("\n");
-				printf("---done---\n");
+				printf("\n---done---\n");
 				printf("Password: %s\n", guess);
 				printf("Found in %d attempts\n", tries);
-
-				clock_t end = clock();
-				double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-				printf("Completed in %f seconds\n", time_spent);
 				return 0;
 			}
 		} while(inc(guess));
 	}
+	return 1;
+}
+
+int main(int argc, char** argv) {
+	clock_t begin = clock();
+
+	//try dictionary attack first
+	//attempt most common passwords
+	if (dictionary_crack()) {
+		//now try brute force
+		//try every possible password
+		if (brute_force_crack()) {
+			//out of guesses
+			printf("Couldn't find password.\n");
+			return 1;
+		} 
+		else printf("Brute force succeeded.\n");
+	}
+	else printf("Dictionary crack succeeded.\n");
+
+	clock_t end = clock();
+	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("Completed in %f seconds.\n", time_spent);
+	return 0;
+
 	//out of guesses
 	printf("Couldn't find password.");
 	return 1;
