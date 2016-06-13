223ba7b990fb49639be336e149f3e1f6a2e61607
Phillip Tennen <phillip.ennen@gmail.com>
2016-06-13 17:44:45 -0600

    Allow cracker to take password as argument

--- cracker.c
+++ cracker.c
@@ -6,17 +6,23 @@
 #include <signal.h>
 #include <time.h>
 
-#define MAX_LENGTH 4
+#define MAX_LENGTH 16
 
-int attempt_password(char* pass) {
-	char input[strlen(pass) + 1];
-	sprintf(input, "./locked \"%s\"\n", pass);
-	return system(input);
+int attempt_password(char* guess, char* correct) {
+	//if they didn't give a password to check against, 
+	//pass their guess to the locked program
+	if (!correct) {
+		char input[strlen(guess) + 1];
+		sprintf(input, "./locked \"%s\"\n", guess);
+		return system(input);
+	}
+	//check for correct password
+	return strcmp(guess, correct);
 }
 
 static int tries = 0;
 
-int dictionary_crack() {
+int dictionary_crack(char* correct) {
 	FILE* file = fopen("common_pwds.txt", "r");
 	char line[256];
 
@@ -29,7 +35,7 @@ int dictionary_crack() {
 			*pos = '\0';
 		}
 
-		if (!attempt_password(line)) {
+		if (!attempt_password(line, correct)) {
 			//found correct password!
 			printf("\n---done---\n");
 			printf("Password: %s\n", line);
@@ -56,7 +62,7 @@ int inc(char* c) {
 	return 1;
 }
 
-int brute_force_crack() {
+int brute_force_crack(char* correct) {
 	for (int length = 1; length <= MAX_LENGTH; length++) {
 		char* guess = malloc((length + 1) * sizeof(char));
 		for (int i = 1; i <= length; i++) {
@@ -67,7 +73,7 @@ int brute_force_crack() {
 		}
 		do {
 			tries++;
-			if (!attempt_password(guess)) {
+			if (!attempt_password(guess, correct)) {
 				//success! we found the correct password
 				printf("\n---done---\n");
 				printf("Password: %s\n", guess);
@@ -82,12 +88,16 @@ int brute_force_crack() {
 int main(int argc, char** argv) {
 	clock_t begin = clock();
 
+	//if the user gave a password to use, use it instead of the locked program
+	char* passed = NULL;
+	if (argc > 1) passed = argv[1];
+
 	//try dictionary attack first
 	//attempt most common passwords
-	if (dictionary_crack()) {
+	if (dictionary_crack(passed)) {
 		//now try brute force
 		//try every possible password
-		if (brute_force_crack()) {
+		if (brute_force_crack(passed)) {
 			//out of guesses
 			printf("Couldn't find password.\n");
 			return 1;
